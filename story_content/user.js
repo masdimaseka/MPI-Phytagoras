009a845a08761392e6007f94c2ed7bd5272a2cdc
masdimaseka <dimasekaid@gmail.com>
2023-07-15 09:21:38 +0800

    v.0.4.0.9 rev(add home and back button on all page, add backsound)

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,17 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5rR6L2PGxuF":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
 }
 
