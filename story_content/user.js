e78211b432c86d04849137840544f8a87dc3e6bd
masdimaseka <dimasekaid@gmail.com>
2023-07-16 08:43:58 +0800

    v.0.4.0.10 rev(Updates on all chapter and introduction page, add pause and play backsound feature)

--- story_content/user.js
+++ story_content/user.js
@@ -2,8 +2,26 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5rR6L2PGxuF":
+      case "6g92SqDhB0a":
         Script1();
+        break;
+      case "6YLnNXqXfn8":
+        Script2();
+        break;
+      case "6LWaQfyA4Cn":
+        Script3();
+        break;
+      case "63pbl1ZZ5Ic":
+        Script4();
+        break;
+      case "62m4jDHirt8":
+        Script5();
+        break;
+      case "6rfJRMG8Cpp":
+        Script6();
+        break;
+      case "5mfLRkKvKOz":
+        Script7();
         break;
   }
 }
@@ -14,5 +32,41 @@ function Script1()
 audio.src="musik.mp3";
 audio.load();
 audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.play();
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.pause();
 }
 
